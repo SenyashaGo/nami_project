348dc6d7a91ab0dd245b9a16fa0b5f8da8e26e3c
Nemo499 <141721123+Nemo499@users.noreply.github.com>
2023-08-28 15:11:26 +0300

    Add files via upload

--- CGenerating_a_report_file.h
+++ CGenerating_a_report_file.h
@@ -6,80 +6,30 @@
 #include "CStruk.h"
 #include "CStruk_input.h"
 #include <stdlib.h>
+#include "CSettingsChannel.h"
 using namespace std;
 class CGenerating_a_report_file
 {
 public:
 	string str;
-	CGenerating_a_report_file(vector < COutputChannel>& Output_book, vector<CStruk>& Struk_book, vector<CStruk_input>& book_Struk_input, string s)
+	CGenerating_a_report_file(vector < COutputChannel>& Struk_book, vector<CSettingsChannel>& book_Struk_input, string s)
 	{
 		ofstream file("D:\\" + s + ".txt");
-		for (int i = 0; i < Output_book.size(); i++)
-		{
-			str += "Порт " + to_string(Output_book[i].nubmer_port) + "\n";
-			if (Output_book[i].cheak_periodicity && Output_book[i].cheak_id && Output_book[i].cheak_msg)
-			{
-				str += "Порт " + to_string(Output_book[i].nubmer_port) + " успешно получил ID " + to_string(Output_book[i].id) + "\n";
-			}
-			else
-			{
-				if (!Output_book[i].cheak_periodicity)
-				{
-					str += "Ошибка! Частота получения сообщения на порту " + to_string(Output_book[i].nubmer_port) + " не совпадает с заданным. ID " + to_string(Output_book[i].id) + "\n";
-					for (int z = 0; z < book_Struk_input.size(); z++)
-					{
-						if (book_Struk_input[z].id == Output_book[i].id)
-						{
-
-							str += "Заданный периуд - " + to_string(book_Struk_input[i].periodicity) + " полученный - " + to_string(Output_book[i].periodicity) + "\n";
-							break;
-						}
-					}
-				}
-				if (!Output_book[i].cheak_msg)
-				{
-					str += "Ошибка! Полученое сообщение на порту " + to_string(Output_book[i].nubmer_port) + " не совпадает с заданным. ID " + to_string(Output_book[i].id) + "\n";
-					str += "Заданое сообщение - ";
-					for (int z = 0; z < book_Struk_input.size(); z++)
-					{
-						if (book_Struk_input[z].id == Output_book[i].id)
-						{
-
-							for (int j = 0; j < 8; j++)
-							{
-								str += book_Struk_input[z].msg[j] + " ";
-							}
-							break;
-						}
-					}
-					str += "; полученное - ";
-					for (int j = 0; j < 8; j++)
-					{
-						str += Output_book[i].msg[j] + " ";
-					}
-					str += "\n";
-				}
-				if (!Output_book[i].cheak_id)
-				{
-					str += "Ошибка! Порт " + to_string(Output_book[i].nubmer_port) + " не нашёл ID\n";
-				}
-			}
-		}
 		for (int i = 0; i < Struk_book.size(); i++)
 		{
-			if (Struk_book[i].msg_ind && Struk_book[i].periodicity_ind && Struk_book[i].port_ind)
+			if (Struk_book[i].cheak_msg && Struk_book[i].cheak_periodicity && Struk_book[i].cheak_id)
 			{
-				str += "Порт " + to_string(Struk_book[i].port) + " успешно получил ID " + to_string(Struk_book[i].id) + "\n";
+				str += "Порт " + to_string(Struk_book[i].nubmer_port) + " успешно получил ID " + to_string(Struk_book[i].id) + "\n";
 			}
-			else if (Struk_book[i].port == -1)
+			else if (Struk_book[i].id==-1)
 			{
-				str += "Ошибка! ID " + to_string(Struk_book[i].id) + " не найден\n";
+				str += "Ошибка! Порт " + to_string(Struk_book[i].nubmer_port) + " не получил id\n";
 			}
 			else
 			{
-				if (!Struk_book[i].msg_ind)
+				if (!Struk_book[i].cheak_msg)
 				{
-					str += "Ошибка! Полученное сообщение на порт " + to_string(Struk_book[i].port) + " не совпадает с заданным\n";
+					str += "Ошибка! Полученное сообщение на порт " + to_string(Struk_book[i].nubmer_port) + " не совпадает с заданным\n";
 					str += "Заданое сообщение - ";
 					for (int z = 0; z < book_Struk_input.size(); z++)
 					{
@@ -100,9 +50,9 @@ public:
 					}
 					str += "\n";
 				}
-				if (!Struk_book[i].periodicity_ind)
+				if (!Struk_book[i].cheak_periodicity)
 				{
-					str += "Ошибка! Частота получения сообщения на порту " + to_string(Struk_book[i].port) + " не совпадает с заданным\n";
+					str += "Ошибка! Частота получения сообщения на порту " + to_string(Struk_book[i].nubmer_port) + " не совпадает с заданным\n";
 					for (int z = 0; z < book_Struk_input.size(); z++)
 					{
 						if (book_Struk_input[z].id == Struk_book[i].id)
@@ -113,6 +63,12 @@ public:
 					}
 				}
 			}
+			for (int z = 0; z < 2; z++)
+			{
+				str += "\n";
+			}
 		}
+		file << str;
+		file.close();
 	}
 };
