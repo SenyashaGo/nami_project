5008a4c68074bbe37a95daeef77368a916b23c1a
Nemo499 <141721123+Nemo499@users.noreply.github.com>
2023-08-28 14:27:55 +0300

    Update CSettingsChannel.h

--- FullChek/CSettingsChannel.h
+++ FullChek/CSettingsChannel.h
@@ -3,7 +3,7 @@
 class CSettingsChannel
 {
 public:
-	CSettingsChannel(int newnubmer_port, long newid, int newbitrate, char newmsg[8], unsigned long newperiodicity);
+	CSettingsChannel(int newnubmer_port, long newid, int newbitrate, char newmsg[8], unsigned long newperiodicity, bool newtype_id);
 	~CSettingsChannel();
 	int SetBitrate(long read_bitrate);
 	int nubmer_port;
